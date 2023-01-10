dff946115d743975c4622ae74431a2ab654da33c
Rafael <rafinhalq@gmail.com>
2023-01-10 20:16:17 -0300

    Criação do mapeamento das entidades

--- ms.order/src/main/java/uol/compass/ms/order/model/ItemEntity.java
+++ ms.order/src/main/java/uol/compass/ms/order/model/ItemEntity.java
@@ -2,7 +2,11 @@ package uol.compass.ms.order.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -16,12 +20,16 @@ import lombok.NoArgsConstructor;
 @Table(name = "item")
 public class ItemEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
 
+    @Column(name = "creation_date")
     private LocalDate creationDate;
-
+    
+    @Column(name = "expiration_date")
     private LocalDate expirationDate;
 
     private Double value;
