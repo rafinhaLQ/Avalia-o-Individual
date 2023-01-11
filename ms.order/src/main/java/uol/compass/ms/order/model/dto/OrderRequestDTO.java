5a907340d7029d0a0f92ac7d8f4f0cc702466070
Rafael <rafinhalq@gmail.com>
2023-01-10 21:53:38 -0300

    Validação no request

--- ms.order/src/main/java/uol/compass/ms/order/model/dto/OrderRequestDTO.java
+++ ms.order/src/main/java/uol/compass/ms/order/model/dto/OrderRequestDTO.java
@@ -1,5 +1,8 @@
 package uol.compass.ms.order.model.dto;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,14 +14,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class OrderRequestDTO {
 
+    @NotNull
+    @Size(min = 11, max = 11)
     private String cpf;
 
+    @NotNull
     private Long idItem;
 
+    @NotNull
     private Double total;
 
+    @NotNull
+    @Size(min = 8, max = 8)
     private String cep;
 
+    @NotNull
     private Integer number;
 
 }
--- ms.order/src/main/java/uol/compass/ms/order/service/OrderServiceImpl.java
+++ ms.order/src/main/java/uol/compass/ms/order/service/OrderServiceImpl.java
@@ -1,13 +1,21 @@
 package uol.compass.ms.order.service;
 
+import com.gtbr.ViaCepClient;
+import com.gtbr.domain.Cep;
+
 import uol.compass.ms.order.model.dto.OrderRequestDTO;
 import uol.compass.ms.order.model.dto.OrderResponseDTO;
+import uol.compass.ms.order.model.entities.OrderEntity;
 
 public class OrderServiceImpl implements OrderService {
 
     @Override
     public OrderResponseDTO create(OrderRequestDTO request) {
-        
+        OrderEntity orderToCreate = new OrderEntity();
+        orderToCreate.setCpf(request.getCpf());
+        // orderToCreate.setItems();
+        orderToCreate.setTotal(request.getTotal());
+        Cep cep = ViaCepClient.findCep(request.getCep());
     }
     
 }
