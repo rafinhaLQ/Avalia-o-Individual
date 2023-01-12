afe36e435338665fa1e890f34a219132e70e0135
Rafael <rafinhalq@gmail.com>
2023-01-12 11:55:49 -0300

    100% dos testes do AddressService

--- ms.order/src/test/java/uol/compass/ms/order/service/AddressServiceImplTest.java
+++ ms.order/src/test/java/uol/compass/ms/order/service/AddressServiceImplTest.java
@@ -1,5 +1,6 @@
 package uol.compass.ms.order.service;
 
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
@@ -13,6 +14,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import uol.compass.ms.order.builder.ScenarioBuilder;
+import uol.compass.ms.order.exceptions.InvalidCepException;
 import uol.compass.ms.order.model.entities.AddressEntity;
 import uol.compass.ms.order.repositories.AddressRepository;
 import uol.compass.ms.order.service.impl.AddressServiceImpl;
@@ -21,6 +23,8 @@ import uol.compass.ms.order.service.impl.AddressServiceImpl;
 public class AddressServiceImplTest {
 
     public static final String CEP = "40140650";
+
+    public static final String INVALID_CEP = "40140659";
     
     @InjectMocks
     private AddressServiceImpl addressService;
@@ -39,6 +43,24 @@ public class AddressServiceImplTest {
         assertNotNull(addressEntity);
         assertEquals("Barra", addressEntity.getDistrict());
         verify(addressRepository).save(any());
+    }
+
+    @Test
+    void shouldCreateAddress_addressAlreadyExists() {
+        AddressEntity address = ScenarioBuilder.builAddressEntity();
+
+        when(addressRepository.findByCepAndNumber(any(), any())).thenReturn(address);
+
+        AddressEntity addressEntity = addressService.createAddressWithCep(CEP, 5);
+        assertNotNull(addressEntity);
+        assertEquals("Barra", addressEntity.getDistrict());
+    }
+
+    @Test
+    void shouldCreateAddress_InvalidCepException() {
+        assertThrows(InvalidCepException.class, () -> {
+            addressService.createAddressWithCep(INVALID_CEP, 5);
+        });
     }
 
 }
