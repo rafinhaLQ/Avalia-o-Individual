1675de697918f8f882ba8c73b93a8db06816385b
Rafael <rafinhalq@gmail.com>
2023-01-25 18:27:16 -0300

    Adicionando data.sql e corrigindo swagger

---
+++ ms.order/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO item (`creation_date`, `description`, `expiration_date`, `name`, `value`) VALUES ('2023-01-11', '1L de leite Piracanjuba', '2023-03-11', 'Leite',   4.49);
+INSERT INTO item (`creation_date`, `description`, `expiration_date`, `name`, `value`) VALUES ('2023-01-05', 'Cookies Bauducco Chocolate', '2023-07-05', 'Cookie',   8.99);
+INSERT INTO item (`creation_date`, `description`, `expiration_date`, `name`, `value`) VALUES ('2023-01-03', 'Queijo Reino Reserva Especial (600g)', '2023-01-018', 'Queijo Reino',   74.49);
+INSERT INTO item (`creation_date`, `description`, `expiration_date`, `name`, `value`) VALUES ('2023-01-25', 'Pao De Forma Integral 400g - Visconti', '2023-02-10', 'Pao de Forma',   6.99);
